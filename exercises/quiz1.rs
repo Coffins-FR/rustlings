6f26eaef9e2232de40b9a706f92c712a508d4acd
Coffins-FR <brian.ziani@gmail.com>
2024-02-06 16:01:26 +0100

    exercices from if to strings

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -1,22 +1,12 @@
-// quiz1.rs
-//
-// This is a quiz for the following sections:
-// - Variables
-// - Functions
-// - If
-//
-// Mary is buying apples. The price of an apple is calculated as follows:
-// - An apple costs 2 rustbucks.
-// - If Mary buys more than 40 apples, each apple only costs 1 rustbuck!
-// Write a function that calculates the price of an order of apples given the
-// quantity bought.
-//
-// No hints this time ;)
-
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(number: i16) -> i16 {
+    if(number <= 40) {
+        return 2 * number
+    }
+    else {
+        return number
+    }
+}
 
 // Don't modify this function!
 #[test]
